2c33d4fdc1c9c84b5bc8478f262de1cd7e48a306
oksananaumova <oksana.naumova@mustpay.tech>
2023-09-15 14:03:53 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/greeting/:name', function (req, res) {
     res.send(`<h1>Wow! Hello there, ${req.params.name} on the page</h1>`);
   });
 
+  app.get('/tip/:total/:tipPercentage', function (req, res) {
+    res.send(`<h1>You total amount is ${req.params.total}$, so you have to give ${req.params.total * req.params.tipPercentage / 100}$ tips.`);
+  });
+
 app.listen(3000, function () {
   console.log('Listening on port 3000');
 });
